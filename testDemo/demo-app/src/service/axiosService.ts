262681104bc369126de4235b05311b936fa4d968
HaiTran <tranvanhoanghai2510@gmail.com>
2022-11-11 10:13:13 +0700

    Remove store

--- testDemo/demo-app/src/service/axiosService.ts
+++ testDemo/demo-app/src/service/axiosService.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
-import store from '../app/store';
 import NotificationUtils from "../component/common/Notification/Notification";
 import _ from "lodash";
-
-const {dispatch} = store;
 
 // Create global Axios instance for CTC Backends API calls
 // Timeout 3,5 sec for each requests, ref: https://stackoverflow.com/questions/266281/best-practices-for-web-service-timeouts
@@ -19,18 +16,14 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(request => {
-    // console.log(request);
-    // Edit request config
     return request;
 }, error => {
     console.log(error);
-    NotificationUtils.error(` ${error}`);
+    NotificationUtils.error(_.get(error, 'request.data.message'));
     return Promise.reject(error);
 });
 
 instance.interceptors.response.use(response => {
-    // console.log(response);
-    // Edit response
     return response;
 }, error => {
     console.log(error);
